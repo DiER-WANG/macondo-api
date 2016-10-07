2054518836cc85ece009f87c2bbd1a4b123da674
王昌阳 <1258866830@qq.com>
2016-10-07 20:10:49 +0800

    add

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
 var express = require('express');
 var app = express();
 var music = require('./music');
+var account = require('./account');
 
 // 获取文件夹内的资源
 app.use('/resource', express.static('resource'));
-
-// example
-// var birds = require('./birds');
-// app.use('/birds', birds);
-app.use('/api/v1/music', music);
-
+app.use('/api/music', music);
+app.use('/api/account', account);
 app.listen(8080, function () {
 	console.log('Macondo\'s backend is starting');
 });
