d4993fd6357f2e6d49566646b82cccd1dd8c60f7
wangchangyang <1258866830@qq.com>
2016-10-08 15:02:53 +0800

    add register

--- account.js
+++ account.js
@@ -1,21 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var db = require('./database');
+
 router.use('/', function (req, res, next) {
-	console.log('account register');
+	console.log('account manage');
 	next();
 })
 // 注册账号
-router.post('/register/:account-:password', function (req, res) {
-	
-	// 先判断账号是否存在；在 数据库 中查找相关信息
-	// 如果存在，返回注册失败
-	// 如果不存在，返回注册成功
+router.post('/register', function (req, res) {
+	console.log(req.body);
+	var body = '';
+	req.on('data', function (dataChunk) {
+		body += dataChunk;
+	});
 
-	res.send(req.params);
+	req.on('end', function () {
+		// 先判断账号是否存在；在 数据库 中查找相关信息
+		// 如果存在，返回注册失败
+		// 如果不存在，返回注册成功
+		var bodyJSON = JSON.parse(body);
+		db.findAccount(bodyJSON, function (documents) {
+			if (documents.length == 0) {			
+				db.insertAccount(bodyJSON, function (err, result) {				
+					console.log('注册成功');
+					res.send(result);
+				});
+			} else {
+				console.log('账号名已存在，请换一个帐号名重新注册');
+				res.send(documents);
+			}		
+		});
+	});
 });
 
+// 登录
 router.get('/login/:account-:password', function (req, res) {
-	res.send('登录成功');
-})
+
+});
 
 module.exports = router;
---
+++ database.js
@@ -0,0 +1,49 @@
+var mongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var connect = function (databaseName, callback) {
+	var url = 'mongodb://localhost:27017/' + databaseName;	
+	console.log(url);
+	mongoClient.connect(url, function (err, database) {		
+		assert.equal(null, err);
+		console.log('Successfully connected to MongoDB instance');
+		callback(database);
+	});
+}
+
+var findOne = function (databaseName, collectionName, target, callback) {
+	connect(databaseName, function (database) {		
+		var collection = database.collection(collectionName);
+		collection.find(target).toArray(function (err, documents) {
+			assert.equal(err, null);
+			callback(documents);
+			database.close();
+		});
+	});
+}
+
+var insertOne = function (databaseName, collectionName, doc, callback) {
+	connect(databaseName, function (database) {		
+		var collection = database.collection(collectionName);
+		collection.insertOne(doc, function (err, result) {
+			assert.equal(null, err);
+			callback(result);
+			database.close();
+		});
+	});
+}
+
+// 查找帐号
+exports.findAccount = function (register, callback) {	
+	findOne('macondo', 'account', {'account': register.account}, function (documents) {
+		callback(documents);
+	});
+}
+
+// 插入帐号
+exports.insertAccount = function (doc, callback) {
+	console.log('注册账号：' + doc);
+	insertOne('macondo', 'account', doc, function (err, result) {
+		callback(result);
+	});
+}
