7038d36e0b6ed9579a1a861b1ad6b6cf95596408
wangchangyang <1258866830@qq.com>
2016-10-08 20:18:33 +0800

    add

--- account.js
+++ account.js
@@ -8,7 +8,6 @@ router.use('/', function (req, res, next) {
 })
 // 注册账号
 router.post('/register', function (req, res) {
-	console.log(req.body);
 	var body = '';
 	req.on('data', function (dataChunk) {
 		body += dataChunk;
@@ -18,23 +17,36 @@ router.post('/register', function (req, res) {
 		// 先判断账号是否存在；在 数据库 中查找相关信息
 		// 如果存在，返回注册失败
 		// 如果不存在，返回注册成功
+		
 		var bodyJSON = JSON.parse(body);// 将字符串 转变 为 json
-		db.findAccount(bodyJSON, function (err, documents) {
+		console.log('bodyJSON: ' + bodyJSON);
+		db.findAccount({'account': 'aaa'}, function (err, documents) {
 			if (err) {
 				res.send({
 					'error': '服务器查找账号时发生错误',
 					'result': null					
 				});
-			} else {
+			} else {				
+
 				if (documents.length == 0) {			
-					db.insertAccount(bodyJSON, function (err, result) {				
-						res.send({
-							'error': '账号写入数据库时发生错误',
-							'result': result
-						});
+					console.log('222' + documents);
+					db.insertAccount(bodyJSON, function (err, result) {										
+						if (err) {
+							res.send({
+								'error': '注册账号时发生错误',
+								'result': null
+							});
+						} else {	
+							console.log('注册成功');
+							res.send({
+								'error': null,
+								'result': result
+							});
+						}
 					});
 				} else {				
-				// 账号名已存在，请换一个帐号名重新注册
+					console.log('333' + documents);
+					// 账号名已存在，请换一个帐号名重新注册
 					res.send({
 						'error': '账号名已存在，请换一个帐号名重新注册',
 						'result': null
